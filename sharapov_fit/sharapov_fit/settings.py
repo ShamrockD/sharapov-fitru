d3dd93574ab10840944765b2eefe091ee4c739f2
ShamrockD <virussotheone@gmail.com>
2025-09-15 16:42:53 +0300

    added prerelease

--- sharapov_fit/sharapov_fit/settings.py
+++ sharapov_fit/sharapov_fit/settings.py
@@ -25,7 +25,8 @@ if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 else:
     allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', '')
-    ALLOWED_HOSTS = allowed_hosts.split(',') if allowed_hosts else []
+    # ALLOWED_HOSTS = allowed_hosts.split(',') if allowed_hosts else []
+    ALLOWED_HOSTS = ['citylag.ru', 'www.citylag.ru', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
